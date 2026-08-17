d0ff013a79ad9129f77a073cb0b092d98173e1aa
shariyarbhuyan-code <shariyarbhuyan@gmail.com>
2026-08-17 19:41:34 +0800

    Add Main class with Student and Lecturer instances

---
+++ week3-4/Main.java
@@ -0,0 +1,11 @@
+package week3_4;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student("Shariar", 22, "DIT12345");
+        Lecturer l = new Lecturer("Dr. Tan", 45, "LEC9988");
+
+        s.display();
+        l.display();
+    }
+}
