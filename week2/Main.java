a9013b49efb46104f0b30fe42e5f6cefb642dc08
shariyarbhuyan-code <shariyarbhuyan@gmail.com>
2026-08-17 19:38:40 +0800

    Create Main.java

---
+++ week2/Main.java
@@ -0,0 +1,8 @@
+package week2;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Shariar", "DIT12345");
+        student.displayInfo();
+    }
+}
